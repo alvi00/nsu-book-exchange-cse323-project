445c0240d3f5d90ed78986a7b851ca821e85a2b4
alvi00 <ahmadfahmid59@gmail.com>
2025-12-05 21:24:38 +0600

    added apk things

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nsu.book.exchange',
+  appName: 'NSU Book Exchange',
+  webDir: 'dist'
+};
+
+export default config;
